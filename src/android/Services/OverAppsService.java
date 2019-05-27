7e10d8cc79ab7ee2dae310e65f9228f3c6e65536
aleksein <a@info.ee>
2019-05-27 17:03:52 +0300

    Change View to use getResources and getIdentifier instead of R.

--- src/android/Services/OverAppsService.java
+++ src/android/Services/OverAppsService.java
@@ -75,8 +75,9 @@ import java.util.Date;
 
          inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
 
-         overAppsHead = inflater.inflate(getResources().getIdentifier("service_over_apps_head", "layout", getPackageName()));
-         overAppsView = inflater.inflate(getResources().getIdentifier("service_over_apps_view", "layout", getPackageName()));
+         //overAppsHead = inflater.inflate(R.layout.service_over_apps_head, null, false);
+         overAppsHead = inflater.inflate(getResources().getIdentifier("service_over_apps_head", "layout", getPackageName()), null, false);
+         overAppsView = inflater.inflate(getResources().getIdentifier("service_over_apps_view", "layout", getPackageName()), null, false);
          webView = (WebView) overAppsView.findViewById(getResources().getIdentifier("webView", "id", getPackageName()));
          imageHead = (ImageView) overAppsHead.findViewById(getResources().getIdentifier("imageHead", "id", getPackageName()));
          imgClose = (ImageView) overAppsView.findViewById(getResources().getIdentifier("imgClose", "id", getPackageName()));
